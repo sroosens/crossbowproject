00b06103639bd174d91ccf3c704a52d65e63355b
sroosens <109092272+sroosens@users.noreply.github.com>
2022-12-12 19:56:02 +0100

    *fix saturne name to saturn *add mouseY inversion parameter

--- Assets/Scripts/SpaceshipMovement.cs
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpaceshipMovement : MonoBehaviour
 {
     public float speed;
     public float rotationX;
     public float rotationY;
+    public bool  invertMouseY;
     
     // Start is called before the first frame update
     void Start()
@@ -18,12 +20,22 @@ public class SpaceshipMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      speed += Input.GetAxis("Vertical")*Time.deltaTime;
-           transform.Translate(0,0,speed);
-        rotationX += Input.GetAxis("Mouse Y");
-        rotationY += Input.GetAxis("Mouse X");
-        rotationX = Mathf.Clamp(rotationX, -90, 90);
-        transform.rotation= Quaternion.Euler(rotationX,rotationY,0);
-       
+        // On applique une vitesse au vaisseau
+        speed += Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(0, 0, speed * 0.02f);
+
+        // Si bouton droit pressé
+        if (Input.GetMouseButton(1))
+        {
+            // On récupère les rotations X, Y de la souris
+            rotationX += Input.GetAxis("Mouse Y") * (invertMouseY ? -1 : 1);
+            rotationY += Input.GetAxis("Mouse X");
+
+            // On limite la rotation X entre -90 deg et +90 deg
+            //rotationX = Mathf.Clamp(rotationX, -90, 90);
+
+            // On effectue une rotation d'Euler sur le vaisseau
+            transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+        }
     }
 }
